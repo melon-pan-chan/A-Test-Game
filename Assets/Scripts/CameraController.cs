a385c09d9f69c4d350e8205bef8b0d06ffd8f2c1
melon-pan-chan <serabiibot@gmail.com>
2020-07-28 20:08:45 +1000

    Started work on a controller script for moving platformers.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,29 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    [SerializeField, Tooltip("The player character to follow.")]
+public class CameraController : MonoBehaviour {
+    [SerializeField, Tooltip ("The player character to follow.")]
     GameObject playerCharacter;
 
-    [SerializeField, Tooltip("The amount the camera's movement speed should be offset.")]
+    [SerializeField, Tooltip ("The amount the camera's movement speed should be offset.")]
     float speedOffset;
 
-    [SerializeField, Tooltip("The amount the camera's position should be offset.")]
+    [SerializeField, Tooltip ("The amount the camera's position should be offset.")]
     Vector2 positionOffset;
 
-    [SerializeField, Tooltip("The amount the camera should pan downwards while crouching.")]
+    [SerializeField, Tooltip ("The amount the camera should pan downwards while crouching.")]
     float crouchDistance;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start () {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update () {
         //Camera's current position
         Vector3 startPosition = transform.position;
         //Player's current position
@@ -36,16 +33,12 @@ public class CameraController : MonoBehaviour
         Vector3 crouchPosition = endPosition;
         crouchPosition.y -= crouchDistance;
 
-
         //Lerp implementation
         //Also a crouch mechanic.
-        if (Input.GetKey("s") || Input.GetKey("down"))
-        {
-            transform.position = Vector3.Lerp(startPosition, crouchPosition, speedOffset * Time.deltaTime);
-        }
-        else
-        {
-            transform.position = Vector3.Lerp(startPosition, endPosition, speedOffset * Time.deltaTime);
+        if (Input.GetKey ("s") || Input.GetKey ("down")) {
+            transform.position = Vector3.Lerp (startPosition, crouchPosition, speedOffset * Time.deltaTime);
+        } else {
+            transform.position = Vector3.Lerp (startPosition, endPosition, speedOffset * Time.deltaTime);
         }
 
         //No Lerp/Smooth damping implementation
---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    //Vector holding the origin point of the platform
+    Vector3 home;
+
+
+    //TODO: Implement a way to select whether the plaform moves horizontally or vertically
+
+    /* Enum signalling what stage of movement the platform is in:
+        0. Home to left
+        1. Left to home
+        2. Home to right
+        3. Right to home
+    */
+    public enum MovementState
+    {
+        HOME_TO_LEFT,
+        LEFT_TO_HOME,
+        HOME_TO_RIGHT,
+        RIGHT_TO_HOME
+    }
+
+    //Instance of the afformentioned MovementState enum
+    [SerializeField, Tooltip("Enum signalling what stage of movement the platform is in")]
+    MovementState currentMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        home = transform.position;
+        currentMovement = MovementState.HOME_TO_LEFT;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Initializing initial position and target position
+        Vector3 startPosition = transform.position;
+        Vector3 endPosition = Vector3.forward; //just a placeholder 
+
+        //Checking whether the 
+
+        //Checking the current movement state so that the appropriate endPosition can be assigned
+        //TODO: Find a way to set the endPosition once and only change it once the movement state changes.
+        switch (currentMovement)
+        {
+            case (MovementState.HOME_TO_LEFT):
+                endPosition = new Vector3(startPosition.x - 1, startPosition.y, startPosition.z);
+                break;
+            case (MovementState.LEFT_TO_HOME):
+                endPosition = new Vector3(startPosition.x + 1, startPosition.y, startPosition.z);
+                break;
+            case (MovementState.HOME_TO_RIGHT):
+                endPosition = new Vector3(startPosition.x + 1, startPosition.y, startPosition.z);
+                break;
+            case (MovementState.RIGHT_TO_HOME):
+                endPosition = new Vector3(startPosition.x - 1, startPosition.y, startPosition.z);
+                break;
+        }
+        transform.position = Vector3.Lerp(startPosition, endPosition, Time.deltaTime);
+    }
+}
